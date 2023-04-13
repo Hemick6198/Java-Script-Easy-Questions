bf2725adb16c09243da2850502a76492bc0900ac
Ethan <ethan.nordahl1@gmail.com>
2023-04-12 22:26:44 -0700

    easy questions

---
+++ app.js
@@ -0,0 +1,195 @@
+// 
+//                      ~ Question - 1 ~
+// 
+// 
+//      Return the Sum of Two Numbers
+// 
+//      Create a function that takes two arguments as functions 
+//      and return their sum.
+// 
+// 
+// function addition(num1, num2) {
+//   return num1 + num2;
+// }
+// 
+// console.log(addition(3, 5));
+// 
+// 
+//                      ~ Question - 2 ~
+// 
+// 
+//      Convert Hours into Seconds
+// 
+//      Create a function that converts hours into seconds.
+// 
+// 
+// function hoursIntoSeconds(hours) {
+//     return hours * 60 * 60;
+// }
+// 
+// console.log(hoursIntoSeconds(24));
+// 
+// 
+//                      ~ Question - 3 ~
+// 
+//      Calculate the Perimeter of a Rectangle
+// 
+//      Create a function that takes length and width of a rectangle
+//      and return it's perimeter.
+// 
+// 
+// function perimeterOfARectangle(length, width){
+//     return length * 2 + width * 2;
+// }
+// 
+// console.log(perimeterOfARectangle(2, 9));
+// 
+// 
+//                      ~ Question - 4 ~
+// 
+//      Calculate the Area of a Triangle
+// 
+//      Write a function that takes the base and height of a triangle
+//      and return its area.
+// 
+// 
+// function calcAreaOfATriangle(base, height) {
+//     return (1 / 2) * base * height;
+// }
+// 
+// console.log(calcAreaOfATriangle(20, 20));
+// 
+// 
+//                      ~ Question - 5 ~
+// 
+//      Extend a String
+// 
+//      Write a function that accepts a string and adds "frontend"
+//      onto the end of it
+// 
+// 
+// function extendAString(fruit) {
+//     return fruit + "Frontend"
+// }
+// 
+// console.log(extendAString("Banana"));
+// 
+// 
+//                      ~ Question - 6 ~
+// 
+//      Greater than 100?
+// 
+//      Given two numbers, return true if the sum of both numbers
+//      is greater than 100. Otherwise, return false.
+// 
+// 
+// function sumGreaterThan100(num1, num2) {
+//     return 100 < num1 + num2  
+// }
+// 
+// console.log(sumGreaterThan100(200, 10));
+// 
+// 
+//                      ~ Question - 7 ~
+// 
+//      Less than or Equal to Zero?
+// 
+//      Write a function that accepts a number and retuns true if it is
+//      less than or equal to zero, otherwise returns false.
+// 
+// 
+// function lessThanOrEqualToZero(num) {
+//     return 0 >= num;
+// }
+// 
+// console.log(lessThanOrEqualToZero(0));
+// 
+// 
+//                      ~ Question - 8 ~
+// 
+//      Opposite boolean
+// 
+//      Given a boolean (true or false), return the opposite boolean.
+// 
+// 
+// function oppositeBoolean(bool) {
+//     return !bool;
+// }
+// 
+// console.log(oppositeBoolean(false));
+// 
+// 
+//                      ~ Question - 9 ~
+// 
+//      Is not the number 0
+// 
+//      Given ANY element, return true if it is NOT the number 0.
+// 
+// 
+// function notNumberZero(num) {
+//     return num !== 0;
+// }
+// 
+// console.log(notNumberZero(0));
+// 
+// 
+//                      ~ Question - 10 ~
+// 
+//      Calculate the remainder
+// 
+//      Given two number, return their remainder when 
+//      divided by each other.
+// 
+// 
+// function calcRemainder(num1, num2) {
+//     return num1 % num2
+// }
+// 
+// console.log(calcRemainder(4, 2));
+// 
+// 
+//                      ~ Question - 11 ~
+// 
+//      Is the number odd?
+// 
+//      Given two numbers, return true if the number is odd.
+// 
+// 
+// function isNumberOdd(num) {
+//     return num % 2 !== 0;
+// }
+// 
+// console.log(isNumberOdd(1));
+// 
+// 
+//                      ~ Question - 12 ~
+// 
+//      If a number is even, return 1 otherwise return -1
+// 
+//      Create a function that takes a number argument and returns 1
+//      if the number is even. If the number is odd return -1.
+// 
+// 
+// function booleanInteger(num) {
+//     return num % 2 == 0 ? 1 : -1;
+// }
+// 
+// console.log(booleanInteger(1));
+// 
+// 
+//                      ~ Question - 13 ~
+//
+//      Check if a user is logged in AND subscribed
+// 
+//      Create a function that takes in two strings. If the first string
+//      is equal to 'LOGGED_IN' AND the second string is equal to 
+//      'SUBSCRIBED' return true, otherwise return false.
+// 
+// 
+// function isLoggedInAndSubscribed(loggedIn, subscribed) {
+//     return (loggedIn === 'LOGGED_IN') && (subscribed === 'SUBSCRIBED')
+// }
+// 
+// console.log(isLoggedInAndSubscribed('LOGGED_IN', 'SUBSCRIBED'));
+// 
+// 
